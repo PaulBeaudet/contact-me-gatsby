844e211d3677cf6083ec3c8c5395464a99bc9851
Paul Beaudet <inof8or@gmail.com>
2020-05-27 12:21:47 -0400

    $1

---
+++ commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copy over important things to dropbox but not bs
+rsync -rac \
+--exclude= .git \
+--exclude= .cache \
+--exclude='node_modules' \
+--exclude='public' \
+--delete \
+./ \
+$HOME/Dropbox/programs/react/contact-me-gatsby
+# End rsync ... wish passing --exclude-from= .gitignore worked
+
+if [ '$1' ]; then
+    git add .
+    git commit -m '$1'
+fi

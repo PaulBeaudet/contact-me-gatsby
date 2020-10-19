7ba1d9a1026266093723d5ca856a02d19bc0531e
Paul Beaudet <inof8or@gmail.com>
2020-10-19 13:53:55 -0400

    Use a seperate db for dev and up the lambda power

--- serve.sh
+++ serve.sh
@@ -2,6 +2,8 @@
 . ./personal.sh # Load private configuration
 
 export MONOLITH="true"
+# Use a different db than prod
+export DB_NAME="dev"
 monoprocesses=`ps aux | grep "monolithic_server.js" | grep "node" | awk '{print $2}'`
 [ "$monoprocesses" ] && kill -9 $monoprocesses
 cd serverless/
